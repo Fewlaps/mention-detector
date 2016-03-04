c793d5ef682ec407a88bca132ee550594cd61b05
Roc Boronat <roc@fewlaps.com>
2016-03-04 17:43:24 +0100

    Extracting the list of punctuation marks to meet the Open Closed Principle

--- src/main/java/com/fewlaps/mentiondetector/RemovePunctuationMarks.java
+++ src/main/java/com/fewlaps/mentiondetector/RemovePunctuationMarks.java
@@ -2,12 +2,15 @@ package com.fewlaps.mentiondetector;
 
 public class RemovePunctuationMarks {
 
+    private static final String EMPTY = "";
+    private static final String[] PUNCTUATION_MARKS = {
+            "!", "\\?", "¡", "¿"
+    };
+
     public String removePunctuationMarks(String text) {
-        return text
-                .replaceAll("!", "")
-                .replaceAll("\\?", "")
-                .replaceAll("¡", "")
-                .replaceAll("¿", "")
-                ;
+        for (String mark : PUNCTUATION_MARKS) {
+            text = text.replaceAll(mark, EMPTY);
+        }
+        return text;
     }
 }
