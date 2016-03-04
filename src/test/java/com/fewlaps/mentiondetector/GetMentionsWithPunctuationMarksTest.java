aace54180bf0d1edda5a0b43c19d1ddff8f65e05
Roc Boronat <roc@fewlaps.com>
2016-03-04 18:13:12 +0100

    Renaming a class to improve its understaindability

--- src/test/java/com/fewlaps/mentiondetector/GetMentionsWithPunctuationMarksTest.java
+++ src/test/java/com/fewlaps/mentiondetector/GetMentionsWithPunctuationMarksTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class GetMentionsWithExclamationMarksTest {
+public class GetMentionsWithPunctuationMarksTest {
 
     @Test
     public void shouldIgnoreExclamationMark() throws InterruptedException {
