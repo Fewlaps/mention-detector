ba91b3634a85118647035c8d576b8d332501b197
Roc Boronat <roc@fewlaps.com>
2016-03-04 21:11:39 +0100

    Using cached Patterns instead of simple String.replaceAll() to remove the symbols to improve the performance

--- src/main/java/com/fewlaps/mentiondetector/RemovePunctuationMarks.java
+++ src/main/java/com/fewlaps/mentiondetector/RemovePunctuationMarks.java
@@ -1,5 +1,9 @@
 package com.fewlaps.mentiondetector;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class RemovePunctuationMarks {
 
     private static final String EMPTY = "";
@@ -7,10 +11,22 @@ public class RemovePunctuationMarks {
             "!", "\\?", "¡", "¿", "\\(", "\\)", "\\{", "\\}", "\\[", "\\]"
     };
 
+    private static List<Pattern> cache;
+
     public String removePunctuationMarks(String text) {
-        for (String mark : PUNCTUATION_MARKS) {
-            text = text.replaceAll(mark, EMPTY);
+        for (Pattern pattern : getPatterns()) {
+            text = pattern.matcher(text).replaceAll(EMPTY);
         }
         return text;
+    }
+
+    private List<Pattern> getPatterns() {
+        if (cache == null) {
+            cache = new ArrayList(PUNCTUATION_MARKS.length);
+            for (String mark : PUNCTUATION_MARKS) {
+                cache.add(Pattern.compile(mark));
+            }
+        }
+        return cache;
     }
 }
