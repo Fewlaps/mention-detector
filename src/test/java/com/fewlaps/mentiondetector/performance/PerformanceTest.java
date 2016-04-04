04f0663a12e91d0db9b4bb5a2330b765bc99cf9c
Roc Boronat <roc@fewlaps.com>
2016-04-04 13:48:16 +0200

    Adding tests that actually checks that the algorithm doesn't take too time

--- src/test/java/com/fewlaps/mentiondetector/performance/PerformanceTest.java
+++ src/test/java/com/fewlaps/mentiondetector/performance/PerformanceTest.java
@@ -3,6 +3,8 @@ package com.fewlaps.mentiondetector.performance;
 import com.fewlaps.mentiondetector.MentionDetector;
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PerformanceTest {
 
     public static final int REPETITIONS = 100000;
@@ -10,6 +12,24 @@ public class PerformanceTest {
     public static final String LOREM_IPSUM_WITHOUT_MENTIONS = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
     public static final String LOREM_IPSUM_WITH_3_MENTIONS = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, @mention1 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. @mention2 Excepteur sint occaecat cupidatat non proident, @mention3 sunt in culpa qui officia deserunt mollit anim id est laborum.";
     public static final String LOREM_IPSUM_WITH_1_MENTION_AT_START = "@banana Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+    @Test
+    public void shouldLastLessThanOneMillisecond_whenCallingGetMentions_withZeroMentions() throws InterruptedException {
+       long millisTaken = getTimeToGetMentions(LOREM_IPSUM_WITHOUT_MENTIONS, 1);
+        assertThat(millisTaken).isLessThan(1);
+    }
+
+    @Test
+    public void shouldLastLessThanOneMillisecond_whenCallingGetMentions_withOneMention() throws InterruptedException {
+        long millisTaken = getTimeToGetMentions(LOREM_IPSUM_WITH_1_MENTION_AT_START, 1);
+        assertThat(millisTaken).isLessThan(1);
+    }
+
+    @Test
+    public void shouldLastLessThanTwoMillis_whenCallingGetMentions_withThreeMentions() throws InterruptedException {
+        long millisTaken = getTimeToGetMentions(LOREM_IPSUM_WITH_3_MENTIONS, 1);
+        assertThat(millisTaken).isLessThan(2);
+    }
 
     @Test
     public void runGetMentionsOnce() throws InterruptedException {
